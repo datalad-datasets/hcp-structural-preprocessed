b36379c00cd8995b86405556ad484bbe3b4e3759
Michael Hanke <michael.hanke@gmail.com>
2020-05-07 08:12:04 +0200

    Facilitate re-BIDSification by running script multiple times
    Would now pick up individual new files, and not stop on presence
    of a previous conversion results.

--- .bids_conversion/convert.sh
+++ .bids_conversion/convert.sh
@@ -4,26 +4,26 @@ set -e -u
 
 # get list of dirs once
 for subdir in [0-9]*; do
-    [ -d $subdir ] || continue
-
     newsub="sub-${subdir}"
     mkdir -p ${newsub}/anat
     # rename files
-    git mv ${subdir}/T1w/T1w_acpc_dc.nii.gz ${newsub}/anat/${newsub}_desc-dc_T1w.nii.gz
+    git mv -fk ${subdir}/T1w/T1w_acpc_dc.nii.gz ${newsub}/anat/${newsub}_desc-dc_T1w.nii.gz || true
     # sub-206828 is missing T1w_acpc_dc_restore
-    git mv ${subdir}/T1w/T1w_acpc_dc_restore.nii.gz ${newsub}/anat/${newsub}_desc-dc-restore_T1w.nii.gz || true
-    git mv ${subdir}/T1w/T1wDividedByT2w.nii.gz ${newsub}/anat/${newsub}_T1wDividedByT2w.nii.gz
+    git mv -fk ${subdir}/T1w/T1w_acpc_dc_restore.nii.gz ${newsub}/anat/${newsub}_desc-dc-restore_T1w.nii.gz || true
+    git mv -fk ${subdir}/T1w/T1wDividedByT2w.nii.gz ${newsub}/anat/${newsub}_T1wDividedByT2w.nii.gz || true
     # sub-204622 is missing T2w_acpc_dc
-    git mv ${subdir}/T1w/T2w_acpc_dc.nii.gz ${newsub}/anat/${newsub}_desc-dc_T2w.nii.gz || true
-    git mv ${subdir}/T1w/T2w_acpc_dc_restore.nii.gz ${newsub}/anat/${newsub}_desc-dc-restore_T2w.nii.gz
+    git mv -fk ${subdir}/T1w/T2w_acpc_dc.nii.gz ${newsub}/anat/${newsub}_desc-dc_T2w.nii.gz || true
+    git mv -fk ${subdir}/T1w/T2w_acpc_dc_restore.nii.gz ${newsub}/anat/${newsub}_desc-dc-restore_T2w.nii.gz || true
     # get rid of the old dir
-    git rm -r ${subdir} > /dev/null || rm -rf ${subdir}
+    git rm -r ${subdir} &> /dev/null || rm -rf ${subdir}
 done
 
 # let git-annex ensure all file pointers are proper
-git annex fsck
+git annex fsck -q
 
-# create dataset_description.json
+# create dataset_description.json, if there is none
+[ -f dataset_description.json ] && exit 0
+
 cat >> dataset_description.json << EOT
 {
     "Name": "HCP Structural Preprocessed",
